26bb525b9979a039c8e6599b3e14929cb6eb3279
Shubham kumar bharti <79shubham88@gmail.com>
2023-09-04 15:53:28 +0530

    Random commit

---
+++ FrontEnd/script.js
@@ -0,0 +1,171 @@
+// accessing the required elements
+const form_container = document.getElementById('form-container');
+const form = document.getElementById('sopForm');
+const isStudiedCanada = document.getElementsByName('studiedCanada');
+const canadianInstitute = document.getElementById('canadianInstitute');
+const canadianProgram = document.getElementById('canadianProgram');
+const canadadetails = document.querySelectorAll('.canadadetail');
+const tuitionPaid = document.getElementsByName('tuitionPaid');
+const tuitionFee = document.getElementById('tuitionFee');
+const tution = document.querySelector('.tution');
+const gicDone = document.getElementsByName('gicDone');
+const gicAmount = document.getElementById('gicAmount');
+const gic = document.querySelector('.gic');
+const submitButton = document.getElementById('submit');
+const resetButton = document.getElementById('reset');
+const loader = document.querySelector('.loader');
+
+
+// Loop through the studiedCanada field to disable or enable the input field of canada Institute and program
+for(let isStudied of isStudiedCanada){
+
+    isStudied.addEventListener('click', () => {
+        if(isStudied.value === 'Yes'){
+            canadianInstitute.disabled = false;
+            canadianProgram.disabled = false;
+            canadadetails.forEach((detail) => {
+                detail.style.display = 'inline-block';
+            });
+        }
+        else{
+            canadianInstitute.disabled = true;
+            canadianProgram.disabled = true;
+            canadadetails.forEach((detail) => {
+                detail.style.display = 'none';
+            });
+        }
+    });
+
+}
+
+
+// Loop through the tutionPaid field to enable or disable the input field of tution fee paid
+for(let tution_paid of tuitionPaid){
+
+    tution_paid.addEventListener('click',() => {
+        if (tution_paid.value === 'Yes') {
+            tuitionFee.disabled = false;
+            tution.style.display = 'inline-block';
+        }
+        else{
+            tuitionFee.disabled = true;
+            tution.style.display = 'none';
+        }
+    });
+
+}
+
+
+// Loop through the gicDone field to enable or disable the input field of GIC fee paid
+for(let gic_done of gicDone){
+
+    gic_done.addEventListener('click',() => {
+        if (gic_done.value === 'Yes') {
+            gicAmount.disabled = false;
+            gic.style.display = 'inline-block';
+        }
+        else{
+            gicAmount.disabled = true;
+            gic.style.display = 'none';
+        }
+    });
+
+}
+
+
+// Check that all the input which are not disabled have some value or not
+function areAllInputsFilled() {
+
+    const inputs = form.querySelectorAll('input[required]:not([disabled])');
+
+    for (const input of inputs) {
+        if (!input.value.trim()) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+
+// Check if all the not disabled field have some value then enable the submit button
+form.addEventListener('input', () => {
+
+    if (areAllInputsFilled()) {
+        submitButton.removeAttribute('disabled');
+    }
+    else{
+        submitButton.setAttribute('disabled', 'disabled');
+    }
+
+});
+
+// url to which the ajax call to be done
+const baseUrl = 'http://localhost:8000/';
+
+// adding a listener function to the submit button
+submitButton.addEventListener('click', submitForm);
+
+// calling an asynchronous submitForm function
+async function submitForm(e) {
+
+    // preven the default submit behaviour
+    e.preventDefault();
+
+    // adding some dynamic styles on submission
+    form_container.style.opacity = '0.3';
+    loader.style.visibility = 'visible';
+
+    // fetch all the input
+    const inputs = form.querySelectorAll('input[required]:not([disabled]):not([type="radio"]), select[required]:not([disabled])');
+    const radioButtons = form.querySelectorAll('input[type="radio"]:checked');    
+    
+    // Create an object to store the data
+    const formData = {};
+    
+    // Loop through the selected input fields and store their values in the formData object
+    inputs.forEach((input) => {
+        formData[input.name] = input.value;
+    });
+    
+    // Loop through the selected radio buttons and store their values in the formData object
+    radioButtons.forEach((radioButton) => {
+        formData[radioButton.name] = radioButton.value;
+    });
+    
+    // Post the form data
+    const res = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(formData)
+    });
+
+    // dynamic adding of styles
+    loader.style.visibility = 'hidden';
+
+
+    // if the res is ok then render success.html page
+    if (res.ok) {
+
+        const successHtmlResponse = await fetch('success.html');
+        
+        const successHtml = await successHtmlResponse.text();
+        
+        // Create a new div element and set its content to the successHtml
+        const successDiv = document.createElement('div');
+        successDiv.innerHTML = successHtml;
+
+        // Replace the existing content of the body with the success content
+        document.body.innerHTML = '';
+        document.body.appendChild(successDiv);
+
+    }
+    else{
+        // Handle the case when the form submission failed
+        console.error('Form submission failed.');
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,135 @@
+// Requiring important modules
+const express = require('express');
+const app = express();
+const port = 8000;
+const nodemailer = require('nodemailer');
+
+app.use(express.static('./FrontEnd'));
+// to convert the data into json object
+app.use(express.json());
+
+// Creating transporter using nodemailer
+const transporter = nodemailer.createTransport(
+    {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: 'false',
+        auth: {
+            user: 'devshubham9455@gmail.com',
+            pass: 'qcgddsyeiinstcwc',
+        }
+    }
+);
+
+// Creating customized Email template
+function customizeSOP(parcel) {
+
+    // checking some fields
+    if(parcel.studiedCanada === 'No'){
+        parcel.canadianInstitute = 'N/A'
+        parcel.canadianProgram = 'N/A'
+    }
+
+    if(parcel.tuitionPaid === 'No'){
+        parcel.tuitionFee = 0;
+    }
+
+    if(parcel.gicDone === 'No'){
+        parcel.gicAmount = 0;
+    }
+
+    // Generate the customized SOP based on the user's data
+    const sopText = `
+        Dear <strong>${parcel.fullName}</strong>,
+        <br>
+        <br>
+
+        Thank you for providing your information. We are pleased to provide you with a customized Statement of Purpose (SOP) based on your details.
+        <br>
+        <br>
+
+        Here are some of your key details:
+        <br>
+        <ol start = "1">
+            <li><strong>Email:</strong> ${parcel.email}</li>
+            <li><strong>Name:</strong> ${parcel.fullName}</li>
+            <li><strong>Age:</strong> ${parcel.age}</li>
+            <li><strong>Highest Level of Education:</strong> ${parcel.educationLevel}</li>
+            <li><strong>Institute name:</strong> ${parcel.educationInstitute}</li>
+            <li><strong>Study field:</strong> ${parcel.educationInstitute}</li>
+            <li><strong>Work experience:</strong> ${parcel.workExperience}</li>
+            <li><strong>Ever studied in Canada:</strong> ${parcel.studiedCanada}</li>
+            <li><strong>Institute Name:</strong> ${parcel.canadianInstitute}</li>
+            <li><strong>Program in which you enrolled:</strong> ${parcel.canadianProgram}</li>
+            <li><strong>Country from which you're applying:</strong> ${parcel.applyingCountry}</li>
+            <li>
+                <strong>English scores</strong>
+                <ul>
+                    <li><strong>Reading:</strong> ${parcel.listeningScore}</li>
+                    <li><strong>Reading:</strong> ${parcel.readingScore}</li>
+                    <li><strong>Speaking:</strong> ${parcel.speakingScore}</li>
+                    <li><strong>Writing:</strong> ${parcel.writingScore}</li>
+                </ul>
+            </li>
+            <li><strong>Did you pay your First year fee:</strong> ${parcel.tuitionPaid}</li>
+            <li><strong>How much did you pay:</strong> ${parcel.tuitionFee}</li>
+            <li><strong>Did you do a GIC:</strong> ${parcel.gicDone}</li>
+            <li><strong>How much did you pay for GIC:</strong> ${parcel.gicAmount}</li>
+        </ol>
+
+        <strong>Your future goals:</strong><br>
+        ${parcel.futureGoals}
+        <br>
+        <br>
+
+        We look forward to assisting you in your journey to achieve your goals in Canada.
+        <br>
+        <br>
+
+        Sincerely,<br>
+        XYZ
+    `;
+
+    return sopText;
+}
+
+app.post('/', (req, res) => {
+    const parcel = req.body;
+
+    // Define the email content
+    const mailOptions = {
+        from: 'devshubham9455@gmail.com',
+        to: parcel.email, // Using the user's provided email
+        subject: 'Your Customized SOP',
+        html: `${customizeSOP(parcel)}`
+    };
+
+    // Sending the email using transporter
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+            res.status(500).send({
+                status: 'error',
+                message: 'Error sending email'
+            });
+        }
+        else {
+            // console.log('Email sent:', info.response);
+            res.status(200).send({
+                status: 'received',
+                message: 'Email sent successfully'
+            });
+        }
+    });
+
+
+    res.status(200).send({
+        status: 'received'
+    })
+});
+
+// defining the port on which the app should listen
+app.listen(port, () => {
+    console.log(`Server is listening at port: ${port}`);
+});
